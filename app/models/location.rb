3ef0052529b21568bb286dde232d632cc2b17cce
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 17:36:44 -0400

    Adds validations and assoc to locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ApplicationRecord
-  
+  has_many :jobs
+
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :city, uniqueness: {scope: :state}
 end
