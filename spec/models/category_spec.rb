628b221ac8b04b19270431c75dc3a3e6d12a8184
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 10:23:02 -0400

    Basic model tests for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,15 @@ require "rails_helper"
 
 Rspec.describe "Category", type: :model do
 
+  describe "Factory" do
+    Factory.create(:category).should be_valid
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "Associations" do
+    it { should have_many(:jobs) }
+  end
 end
