fa33085e2a60beda440428d57723d2188ded94c6
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 08:19:39 -0400

    Adds endpoint describe blocks

--- spec/request/jobs_controller_spec.rb
+++ spec/request/jobs_controller_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 Rspec.describe "JobsController", type: :request do
-  
+  describe 'GET /jobs' do
+
+  end
+
+  describe 'POST /jobs' do
+
+  end
+
+  describe 'GET /jobs/:id' do
+
+  end
+
+  describe 'PUT /jobs/:id' do
+
+  end
+
+  describe 'DELETE /jobs/:id' do
+
+  end
 end
