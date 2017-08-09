7c3f2aeefe4a14052eabdaa1ca9d21f660d96a7c
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 17:19:43 -0400

    Creates locations factory

---
+++ spec/factories/locations.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :location do
+    place = [{city: "Brooklyn", state: "NY"}, {city: "Cleveland", state: "OH"}].sample
+
+    city { place[:city] }
+    state { place[:state] }
+
+    initialize_with { Category.find_or_create_by(city: city, state: state) }
+  end
+end
