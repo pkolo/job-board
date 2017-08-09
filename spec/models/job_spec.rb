9611ff073b1616314959f8c18ab725a5dfa263d3
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 10:25:53 -0400

    Basic model tests for job

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 Rspec.describe "Job", type: :model do
+  describe "Factory" do
+    Factory.create(:job).should be_valid
+  end
 
+  describe "Validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:details) }
+  end
+
+  describe "Associations" do
+    it { should belong_to(:category) }
+  end
 end
