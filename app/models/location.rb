18420e7256203de147fa52b26777dc6e64e92b28
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 17:40:07 -0400

    Adds nice output method

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,8 @@ class Location < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :city, uniqueness: {scope: :state}
+
+  def to_s
+    "#{self.city}, #{self.state}"
+  end
 end
