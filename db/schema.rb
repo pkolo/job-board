bc7083d7ddc17773358c74c9473efae8ac0c90af
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 10:09:56 -0400

    Generates job model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809140540) do
+ActiveRecord::Schema.define(version: 20170809140908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,16 @@ ActiveRecord::Schema.define(version: 20170809140540) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "details"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_jobs_on_category_id", using: :btree
+  end
+
+  add_foreign_key "jobs", "categories"
 end
