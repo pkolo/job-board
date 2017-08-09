968e54de240935f77d8db7fb366beeae129ccbc2
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 14:13:52 -0400

    Adds basic tests for index route request

--- spec/requests/jobs_controller_spec.rb
+++ spec/requests/jobs_controller_spec.rb
@@ -2,9 +2,38 @@ require "rails_helper"
 
 RSpec.describe "JobsController", type: :request do
   # Initialize data
-  let(:jobs) { FactoryGirl.create_list(:job, 10) }
+  let(:jobs) { FactoryGirl.create_list(:job, 5) }
 
   describe 'GET /jobs' do
+    # make HTTP get request before each example
+    before { get '/api/jobs' }
+    let(:parsed_response) { JSON.parse(response.body) }
+
+    it "returns a success response code" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns json" do
+      expect(response.content_type).to eq("application/json")
+    end
+
+    it "contains meta" do
+      expect(parsed_response["status"]).not_to be nil
+      expect(parsed_response["code"]).not_to be nil
+      expect(parsed_response["messages"]).not_to be nil
+      expect(parsed_response["result"]).not_to be nil
+    end
+
+    it "returns properly formatted jobs as result" do
+      parsed_response["results"].each do |result|
+        expect(result["id"]).not_to be nil
+        expect(result["title"]).not_to be nil
+        expect(result["details"]).not_to be nil
+        expect(result["date_posted"]).not_to be nil
+        expect(result["category"]).not_to be nil
+        expect(result["city"]).not_to be nil
+      end
+    end
 
   end
 
