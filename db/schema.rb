7953fef02e8ddb833392f414ebb7ed30075a727e
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 16:57:40 -0400

    Removes address from jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809140908) do
+ActiveRecord::Schema.define(version: 20170809205435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 20170809140908) do
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "details"
-    t.float    "latitude"
-    t.float    "longitude"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
