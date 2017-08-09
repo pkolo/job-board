6d244deb60858b7a7d6c9be711c0b8adbea1c92d
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 14:57:14 -0400

    Creates first job serializer

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,21 @@ class Job < ApplicationRecord
 
   validates :title, presence: true
   validates :details, presence: true
+
+  # Formats a hash to be converted to JSON and returned by the API.
+  def serialize
+    {
+      id: self.id,
+      title: self.title,
+      details: self.details,
+      date_posted: self.nice_date,
+    }
+  end
+
+  # Formats the creation date into a string
+  def nice_date
+    d = self.created_at
+    d.strftime("%m/%d/%Y")
+  end
+
 end
