718ed283138ef108dfb8c8235a464749df3cfbe5
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-09 11:12:50 -0400

    Adds uniqeness test for category name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Category, type: :model do
 
   describe "Validations" do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 
   describe "Associations" do
