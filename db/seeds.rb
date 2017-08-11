af9f1996f5dcc581aaf9a214c6a3fd07f000af41
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-11 11:25:23 -0400

    Create database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+locations = [
+  {city: "Brooklyn", state: "NY"},
+  {city: "Cleveland", state: "OH"},
+  {city: "Brooklyn", state: "OH"},
+  {city: "Rochester", state: "NY"},
+  {city: "Rochester", state: "MI"},
+  {city: "Toledo", state: "OH"}
+]
+
+categories = ["Home", "Repair", "Garden", "Construction"]
+
+20.times do
+  job_params = {
+    title: Faker::Lorem.sentence,
+    details: Faker::Lorem.paragraph,
+    category_name: categories.sample,
+    location_attributes: locations.sample
+  }
+
+    job = Job.create(job_params)
+
+    job.created_at = rand(0..7).days.ago
+    job.save
+end
