18c3da258ce4bb930fffa85a9877fc33896f5d91
Patrick Kolodgy <pkolodgy@gmail.com>
2017-08-14 17:03:26 -0400

    Improves controller readability

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -5,41 +5,27 @@ class Api::JobsController < ApplicationController
   def index
     jobs = Job.all
     render :json =>
-      {
-        code: 200,
-        messages: [],
-        result: jobs
-      }, status: :ok
+      { code: 200, messages: [], result: jobs },
+        status: :ok
   end
 
   def show
     @job = Job.find(params[:id])
-    if @job.save
-      render :json =>
-      {
-        code: 200,
-        messages: [],
-        result: @job
-      }, status: :ok
-    end
+    render :json =>
+      { code: 200, messages: [], result: @job },
+        status: :ok
   end
 
   def create
     @job = Job.new(job_params)
     if @job.save
       render :json =>
-      {
-        code: 201,
-        messages: [],
-        result: @job
-      }, status: :created
+      { code: 201, messages: [], result: @job },
+        status: :created
     else
       render :json =>
-      {
-        code: 422,
-        messages: @job.errors.full_messages,
-        result: []
-      }, status: :unprocessable_entity
+      { code: 422, messages: @job.errors.full_messages, result: [] },
+        status: :unprocessable_entity
     end
   end
 
@@ -48,18 +34,12 @@ class Api::JobsController < ApplicationController
     @job.update_attributes(job_params)
     if @job.save
       render :json =>
-      {
-        code: 200,
-        messages: ["Job successfully updated."],
-        result: @job
-      }, status: :ok
+      { code: 200, messages: ["Job successfully updated."], result: @job },
+        status: :ok
     else
       render :json =>
-      {
-        code: 422,
-        messages: @job.errors.full_messages,
-        result: []
-      }, status: :unprocessable_entity
+      { code: 422, messages: @job.errors.full_messages, result: [] },
+        status: :unprocessable_entity
     end
   end
 
@@ -67,21 +47,15 @@ class Api::JobsController < ApplicationController
     @job = Job.find(params[:id])
     if @job.destroy
       render :json =>
-      {
-        code: 200,
-        messages: ["Job successfully deleted."],
-        result: []
-      }, status: :ok
+      { code: 200, messages: ["Job successfully deleted."], result: [] },
+        status: :ok
     end
   end
 
   def not_found
     render :json =>
-    {
-      code: 404,
-      messages: ["Record not found."],
-      result: []
-    }, status: :not_found
+    { code: 404, messages: ["Record not found."], result: [] },
+      status: :not_found
   end
 
   private
